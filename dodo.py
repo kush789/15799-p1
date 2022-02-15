e08edd6409120955bf171297a93f85188fedc9fd
Kushagra Singh <singh.kushagra.1996@gmail.com>
2022-02-15 15:04:10 -0500

    revert

--- dodo.py
+++ dodo.py
@@ -3,6 +3,7 @@ def task_project1():
         # A list of actions. This can be bash or Python callables.
         "actions": [
             'echo "Faking action generation."',
+            'echo "SELECT 1;" > actions.sql',
             'echo \'{"VACUUM": true}\' > config.json',
         ],
         # Always rerun this task.
