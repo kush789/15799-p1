0bdc2c2f32705e49375c86297a1ec1b4e250268e
Kushagra Singh <singh.kushagra.1996@gmail.com>
2022-02-15 15:11:24 -0500

    revert

--- dodo.py
+++ dodo.py
@@ -4,6 +4,13 @@ def task_project1():
         "actions": [
             'echo "Faking action generation."',
             'echo "SELECT 1;" > actions.sql',
+            'echo "CREATE INDEX idx_rating_uid ON review (u_id);" > actions.sql',
+            'echo "CREATE INDEX idx_rating_aid ON review (a_id);" >> actions.sql',
+            'echo "CREATE INDEX idx_rating_iid ON review (i_id);" >> actions.sql',
+            'echo "CREATE INDEX idx_review_rating_uid ON review_rating (u_id);" >> actions.sql',
+            'echo "CREATE INDEX idx_review_rating_aid ON review_rating (a_id);" >> actions.sql',
+            'echo "CREATE INDEX idx_trust_sid ON trust (source_u_id);" >> actions.sql',
+            'echo "CREATE INDEX idx_trust_tid ON trust (target_u_id);" >> actions.sql',
             'echo \'{"VACUUM": true}\' > config.json',
         ],
         # Always rerun this task.
