c65da0e091a31c20a4afdaa9c463b755c95add11
Kushagra Singh <singh.kushagra.1996@gmail.com>
2022-03-15 12:48:42 -0400

    added drop index command

--- dodo.py
+++ dodo.py
@@ -11,17 +11,32 @@ def task_project1_setup():
             'sudo pip3 install sqlparse', 
             'sudo pip3 install sql-metadata',
             'sudo pip3 install pprintpp',
+
+            # Figure out existing indexes without constraints
             '''
-                echo "select idx.relname as index_name, pgis.indexdef as index_definition
-                from pg_index pgi
-                join pg_class idx on idx.oid = pgi.indexrelid
-                join pg_namespace insp on insp.oid = idx.relnamespace
-                join pg_class tbl on tbl.oid = pgi.indrelid
-                join pg_namespace tnsp on tnsp.oid = tbl.relnamespace
-                join pg_indexes pgis on pgis.indexname = idx.relname
-                where not pgi.indisunique and not pgi.indisprimary and not pgi.indisexclusion and tnsp.nspname = 'public';" | sudo -u postgres psql project1db > existing_indices.txt
+                echo "select idx.relname as index_name
+                        from pg_index pgi
+                        join pg_class idx on idx.oid = pgi.indexrelid
+                        join pg_namespace insp on insp.oid = idx.relnamespace
+                        join pg_class tbl on tbl.oid = pgi.indrelid
+                        join pg_namespace tnsp on tnsp.oid = tbl.relnamespace
+                        join pg_indexes pgis on pgis.indexname = idx.relname
+                        where not pgi.indisunique and 
+                              not pgi.indisprimary and 
+                              not pgi.indisexclusion 
+                              and tnsp.nspname = 'public';" \
+                | sudo -u postgres psql project1db  \
+                | tail -n +3 \
+                | head -n -2 \
+                | awk '{ print "DROP INDEX" $0 ";"}' \
+                > drop_existing_indices.sql
             ''',
-            'cat existing_indices.txt'
+
+            # Just for debug
+            'cat drop_existing_indices.sql',
+
+            # drop all existing indices without constraints
+            'cat drop_existing_indices.sql | sudo -u postgres psql project1db'
         ],
         "verbosity": 2
     }
